b5799bda60bbc24312d2710ef088afca9db6fbc5
Veronika <47883324+landerman@users.noreply.github.com>
2022-07-01 08:38:02 +0300

    исправила неточности

--- webpack-config/src/index.js
+++ webpack-config/src/index.js
@@ -33,20 +33,56 @@ let goSlider = function(sliderClass, width, elClass ) {
 	}
 }
 
-const checkboxChange = function(checkboxName, checkboxImage, checkboxText, checkboxWrapper, height) {
-	checkboxName.addEventListener('change', function (evt) {
-		evt.preventDefault();
-		if (checkboxImage.classList.contains('checkbox__transform')) {
-			checkboxImage.classList.remove('checkbox__transform');
-			checkboxText.innerHTML = 'Показать все';
-			checkboxWrapper.style = `height: ${height}px`
-		} else {
-			checkboxImage.classList.add('checkbox__transform');
-			checkboxText.innerHTML = 'Скрыть'
-			
-			checkboxWrapper.style = 'height: fit-content'
-		}
-	})
+function blockClick(className) {
+    document.addEventListener("click",handler,true);
+    function handler(e){
+    if(e.target.className!==className)
+    e.stopPropagation()
+}
+}
+
+var btns = document.querySelectorAll(".menu__langs--link");
+
+for (var i = 0; i < btns.length; i++) {
+
+    let variable = btns[i];
+    const ruLang = btns[0];
+    const enLang = btns[1];
+    const chLang = btns[2];
+
+    variable.addEventListener("click", function () {
+        if (!variable.classList.contains('active')) {
+            if (variable === ruLang) {
+                enLang.classList.remove('active')
+                chLang.classList.remove('active')
+                ruLang.classList.add('active')
+            } else if (variable === enLang) {
+                ruLang.classList.remove('active')
+                chLang.classList.remove('active')
+                enLang.classList.add('active')
+            } else if (variable === chLang) {
+                ruLang.classList.remove('active')
+                enLang.classList.remove('active')
+                chLang.classList.add('active')
+            }
+        }
+        variable.classList.add('active')
+    });
+}
+const checkboxChange = function (checkboxName,  checkboxText, checkboxWrapper, height) {
+    checkboxName.addEventListener('change', function (evt) {
+        evt.preventDefault();
+        if ( checkboxText.classList.contains('checkbox__read--transform')) {
+             checkboxText.classList.remove('checkbox__read--transform');
+            checkboxText.innerHTML = 'Показать все';
+            checkboxWrapper.style = `height: ${height}px`
+        } else {
+             checkboxText.classList.add('checkbox__read--transform');
+            checkboxText.innerHTML = 'Скрыть'
+
+            checkboxWrapper.style = 'height: fit-content'
+        }
+    })
 
 }
 
@@ -56,30 +92,27 @@ goSlider(sliderServices, 240, '.tehnic-services__pagination')
 
 
 let tehnicServicesCheckbox = document.querySelector('.tehnic-services__checkbox');
-let tehnicServicesCheckboxImage = document.querySelector('.tehnic-services__checkbox--image');
 let tehnicServicesWrapper = document.querySelector('.tehnic-services__wrapper')
 let tehnicServicesCheckboxText = document.querySelector('.tehnic-services__checkbox--read');
 
-checkboxChange(tehnicServicesCheckbox, tehnicServicesCheckboxImage,tehnicServicesCheckboxText,  tehnicServicesWrapper, 160)
+checkboxChange(tehnicServicesCheckbox, tehnicServicesCheckboxText,tehnicServicesWrapper, 160)
 
 const checkboxMain = document.querySelector('.main-page__checkbox');
-const checkboxImageMain = document.querySelector('.main-page__checkbox--image');
 const checkboxTextMain = document.querySelector('.main-page__checkbox--read');
 const contentText = document.querySelector('.main-page__content-text');
 
-checkboxChange(checkboxMain,checkboxImageMain,checkboxTextMain,  contentText, 139)
+checkboxChange(checkboxMain, checkboxTextMain,  contentText, 139)
 
 const sliderBrands = document.querySelector('.swiper-brand__container');
 
-goSlider(sliderBrands, 240, '.swiper-brand__pagination')
+goSlider(sliderBrands, 240, '.brand__pagination')
 
 
 let checkboxBrands = document.querySelector('.brands__checkbox');
-let checkboxImageBrands = document.querySelector('.brands__checkbox--image');
 let CheckboxTextBrands = document.querySelector('.brands__checkbox--read');
 let wrapperBrands = document.querySelector('.swiper-brand__wrapper');
 
-checkboxChange(checkboxBrands, checkboxImageBrands,CheckboxTextBrands, wrapperBrands, 160);
+checkboxChange(checkboxBrands, CheckboxTextBrands, wrapperBrands, 160);
 
 const sliderPrice = document.querySelector('.price__container');
 goSlider(sliderPrice, 260, '.price__pagination')
@@ -118,21 +151,6 @@ menuExit.addEventListener('click', function (evt) {
         cont.classList.remove('container--filter');
     }
 })
-
-document.addEventListener('click', (e) => {
-    const clickBurger = e.composedPath().includes(burger);
-    const clickMenu = e.composedPath().includes(menu);
-    
-    if (menu.classList.contains('menu--open') && !clickBurger && !clickMenu) {
-        menu.classList.remove('menu--open');
-        for (let i = 0; i < container.length; i++) { // 
-            let cont = container[i];
-
-            cont.classList.remove('container--filter')
-        }
-    }
-})
-
 
 
 let filterAll = function(){
@@ -179,7 +197,7 @@ call2.addEventListener('click', function (evt) {
     
 })
 
-let clickFeedback = function(){
+const clickFeedback = function(){
     if (modal.classList.contains('modal--remove')) {
         modal.classList.remove('modal--remove')
     }
@@ -219,14 +237,54 @@ closeBtn2.addEventListener('click', function (evt) {
 
 })
 
-document.addEventListener('click', (e) => {
-    const clickMOdal = e.composedPath().includes(modal);
-    const clickCall1 = e.composedPath().includes(call1);
-    const clickFeedback1 = e.composedPath().includes(feedback1);
-    const clickCall2 = e.composedPath().includes(call2);
-    const clickFeedback2 = e.composedPath().includes(feedback2);
-    if (!modal.classList.contains('modal--remove') && !clickMOdal && !clickCall1 && !clickFeedback1 && !clickCall2 && !clickFeedback2) {
-        modal.classList.add('modal--remove');
-        filterAll()
+const page = document.querySelector(".page");
+const body = document.querySelector("body");
+
+function closeOverlay() {
+
+    document.addEventListener('click', (e) => {
+        
+        const clickMOdal = e.composedPath().includes(modal);
+        const clickCall1 = e.composedPath().includes(call1);
+        const clickFeedback1 = e.composedPath().includes(feedback1);
+        const clickCall2 = e.composedPath().includes(call2);
+        const clickFeedback2 = e.composedPath().includes(feedback2);
+        const clickBurger = e.composedPath().includes(burger);
+        const clickMenu = e.composedPath().includes(menu);
+    
+        if (!modal.classList.contains('modal--remove') && !clickMOdal && !clickCall1 && !clickFeedback1 && !clickCall2 && !clickFeedback2) {
+    
+            modal.classList.add('modal--remove');
+            filterAll();
+           
+            
+        }else if (menu.classList.contains('menu--open') && !clickBurger && !clickMenu) {
+            menu.classList.remove('menu--open');
+            for (let i = 0; i < container.length; i++) { // 
+                let cont = container[i];
+    
+                cont.classList.remove('container--filter')
+            }
+        }
+    
+       if(!modal.classList.contains('modal--remove')){
+            menu.classList.remove('menu--open');
+        }
+    
+        if (menu.classList.contains('menu--open') || !modal.classList.contains('modal--remove')) {
+          page.classList.add('page--overlay-open')
+          page.classList.remove('page--overlay-close')
+        }else{
+            page.classList.add('page--overlay-close')
+            page.classList.remove('page--overlay-open')
+            
+        }
+        
+        
+    })
+    
+    
     }
-})
+    
+    closeOverlay()
+    
